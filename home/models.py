e59325b128c58e33b75c6430ac7dbdebaa76a8e1
AnujAcharya2338 <anujacharya877@gmail.com>
2025-12-09 08:08:07 +0545

    models in django

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField(default=18)
+    email = models.EmailField()
+    address = models.TextField()
+    image = models.ImageField()
+    file = models.FileField()
+
+class product(models.Model):
+    pass 
